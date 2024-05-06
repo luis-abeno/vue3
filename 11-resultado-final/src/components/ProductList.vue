7f038b4f9d3ebcd27a32aa0d59c2614043b4ed7d
Luis Henrique Tetsuo Abeno de Mambro <luis.mambro@onr.org.br>
2024-05-06 09:27:28 -0300

    feat: some changes

--- 11-resultado-final/src/components/ProductList.vue
+++ 11-resultado-final/src/components/ProductList.vue
@@ -1,10 +1,24 @@
 <template>
   <div class="flex justify-center flex-col items-center mt-4">
-    <ul v-if="storeProduct.products.length > 0">
-      <li v-for="product in storeProduct.products" :key="product.id" class="underline">
-        <router-link :to="`/products/${product.id}`">{{ product.name }}</router-link>
-      </li>
-    </ul>
+    <div v-if="storeProduct.products.length > 0">
+      <div class="grid grid-cols-3 gap-4" v-if="props.type === 'card'">
+        <div
+          v-for="product in storeProduct.products"
+          :key="product.id"
+          class="border p-4 rounded-lg"
+        >
+          <router-link :to="`/products/${product.id}`" class="text-blue-500 hover:underline">{{
+            product.name
+          }}</router-link>
+        </div>
+      </div>
+
+      <ul v-else>
+        <li v-for="product in storeProduct.products" :key="product.id" class="underline">
+          <router-link :to="`/products/${product.id}`">{{ product.name }}</router-link>
+        </li>
+      </ul>
+    </div>
 
     <p v-else>Nenhum produto cadastrado</p>
   </div>
@@ -13,4 +27,11 @@
 <script setup lang="ts">
 import { useProductStore } from '@/stores/product.store'
 const storeProduct = useProductStore()
+
+const props = defineProps({
+  type: {
+    type: String,
+    default: 'card'
+  }
+})
 </script>
--- 11-resultado-final/src/views/ManageProductsView.vue
+++ 11-resultado-final/src/views/ManageProductsView.vue
@@ -8,11 +8,7 @@
     </div>
 
     <div class="flex justify-center flex-col items-center mt-4">
-      <ul v-if="storeProduct.products.length > 0">
-        <li v-for="product in storeProduct.products" :key="product.id" class="underline">
-          <router-link :to="`/products/${product.id}`">{{ product.name }}</router-link>
-        </li>
-      </ul>
+      <ProductList v-if="storeProduct.products.length > 0" type="list" />
 
       <p v-else>Nenhum produto cadastrado</p>
     </div>
@@ -22,6 +18,7 @@
 <script setup lang="ts">
 import { useProductStore } from '@/stores/product.store'
 import { ref } from 'vue'
+import ProductList from '@/components/ProductList.vue'
 
 const storeProduct = useProductStore()
 const product = ref('')
